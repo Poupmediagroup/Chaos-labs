f33826663750c87983552aea6a0c8f0ae8c77267
Tai-Dev <tai-dev@Tai-Devs-MacBook-Pro.local>
2025-03-08 12:55:08 -0600

    added latest provider & created build templates directory

--- build/provider.tf
+++ build/provider.tf
@@ -2,11 +2,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source = "hashicorp/azurerm"
+      version = "4.22.0"
     }
   }
-  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
