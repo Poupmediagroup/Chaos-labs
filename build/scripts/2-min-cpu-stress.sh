2d8446b9e07e4319b84eab3de787ab26c0096cc1
Tai-Dev <tai-dev@Tai-Devs-MacBook-Pro.local>
2025-03-15 20:51:54 -0500

    add basic stress test script

--- build/scripts/2-min-cpu-stress.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Stressing cpu for 2 mins"
-stress 
-    --cpu 2
-    --timeout 120s
-    --verbose
--- build/scripts/stress-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-echo "Creating a docker container that contains the stress utility...."
-docker run -ti --rm polinux/stress bash 
