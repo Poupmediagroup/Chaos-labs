c59bcd3eec4370be94ce1df3e7ea6bbb75b9281f
Tai-Dev <tai-dev@Tai-Devs-MacBook-Pro.local>
2025-03-19 22:01:46 -0500

    add API call for data source import

--- build/scripts/grafana-setup.py
+++ build/scripts/grafana-setup.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+import requests
+import base64
 
 username = 'admin'
 password = 'chaos'
@@ -12,10 +13,11 @@ encoded_grafana_creds = base64.b64encode(grafana_creds.encode('utf-8')).decode('
 # Get the public IP address using ipinfo.io
 ipinfo_response = requests.get('https://ipinfo.io/ip')
 
+# response.text gives the plain IP without JSON structure
+host_ip = ipinfo_response.text.strip()
 
-host_ip = ipinfo_response.text.strip()  # response.text gives the plain IP without JSON structure
 
-# Post request for creating Grafana service account
+# Create Grafana Service Account
 url = f'http://{host_ip}:3000/api/serviceaccounts'
 service_acct_headers = {
         'Content-Type': 'application/json',
@@ -33,8 +35,8 @@ data = service_acct_response.json()
 
 id = data.get('id')
 name = data.get('name')
+
 print(f'Status Code: {service_acct_response.status_code}')
-print(f'Response JSON: {service_acct_response.json()}')
 
 print(f'Service account ID: {id}')
 print(f'Service account Name: {name}')
@@ -42,7 +44,7 @@ print(f'Service account Name: {name}')
 
 # Create Service account token
 
-url = f'http://{host_ip}:3000/api/serviceaccounts/{id}/tokens'
+service_acct_url = f'http://{host_ip}:3000/api/serviceaccounts/{id}/tokens'
 service_acct_headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Basic {encoded_grafana_creds}',
@@ -52,9 +54,35 @@ service_acct_token = {
         'secondsToLive': 0
  }
 
-service_acct_token_response = requests.post(url,headers=service_acct_headers,json=service_acct_token)
+service_acct_token_response = requests.post(service_acct_url,headers=service_acct_headers,json=service_acct_token)
 
 token_data = service_acct_token_response.json()
 key = token_data.get('key')
 print(f'Status Code: {service_acct_token_response.status_code}')
 print(f'Token is: {key}')
+
+
+# Add Node Exporter Data Source
+
+data_source_url = f'http://{host_ip}:3000/api/datasources'
+data_source_headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {key}',
+ }
+data_source_body = {
+        'name': 'Prometheus',
+        'type': 'prometheus',
+        'url': f'http://{host_ip}:9090',
+        'access': 'proxy',
+        'basicAuth': False
+ }
+
+data_source_response = requests.post(data_source_url,headers=data_source_headers,json=data_source_body)
+
+type_data = data_source_response.json()
+
+name = type_data.get('name')
+
+print(f'Response is: {type_data}')
+print(f'Status Code for datasource import: {data_source_response.status_code}')
+print(f'Data Source: {name} has been added')
