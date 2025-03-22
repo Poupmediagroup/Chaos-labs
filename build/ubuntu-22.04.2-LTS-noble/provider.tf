a17a2e2a024b33ef584aed55bbac528311f920cc
Tai Harrell <tharrell@hawk.iit.edu>
2025-03-22 15:13:43 -0500

    upgrade to version 4.24

--- build/ubuntu-22.04.2-LTS-noble/provider.tf
+++ build/ubuntu-22.04.2-LTS-noble/provider.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.22.0"
+      source  = "hashicorp/azurerm"
+      version = "4.24.0"
     }
   }
 }
