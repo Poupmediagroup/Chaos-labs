b9283c3e735863a41569a189973846a920eea9d6
trell991 <taiharrell991@gmail.com>
2025-03-27 22:09:44 -0500

    add broken golden grafana terraform deployment. Packer working

--- build/terraform/golden-grafana/main.tf
+++ build/terraform/golden-grafana/main.tf
@@ -1,18 +1,92 @@
-resource "azurerm_dev_test_virtual_machine" "dtl_vm" {
-  name                = "my-dtl-vm"
-  lab_name            = var.lab_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = var.vm_size
-
-  # Reference the Packer image from DevTest Labs
-  lab_virtual_machine_create_option = "FromCustomImage"
-  lab_custom_image_id               = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.DevTestLab/labs/${var.lab_name}/customimages/${var.image_name}"
-
-  network_interface {
-    name    = "${var.vm_name}-nic"
-    shared_public_ip_address = false
-  }
-
+# Reference the existing managed image
+data "azurerm_image" "existing_image" {
+  name                = var.image_name
+  resource_group_name = var.managed_image_resource_group
 }
 
+# Create a virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "test-net"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = "test-net"
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a public IP
+resource "azurerm_public_ip" "example" {
+  name                = "test-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "example" {
+  name                = "test-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Create the virtual machine using the managed image
+resource "azurerm_linux_virtual_machine" "example" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = var.size
+
+  # Use the managed image
+  source_image_id = data.azurerm_image.existing_image.id
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  # Specify your admin credentials
+  admin_username = "adminuser"
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("/Users/tai-dev/.ssh/terraform_key.pub")
+  }
+}
+
+# Optional: Network security group
+resource "azurerm_network_security_group" "example" {
+  name                = "test-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
--- build/terraform/golden-grafana/variables.tf
+++ build/terraform/golden-grafana/variables.tf
@@ -1,21 +1,42 @@
 variable "subscription_id" {
   type    = string
-  default = ""
-} 
+  default = "42321656-24c7-449a-85cb-e5aeb9b9659b"
+}
 
-variable "lab_name" {
+
+variable "image_name" {
   type    = string
-  default = "Packer-lab"
+  default = "golden-grafana"
 }
 
 
 variable "location" {
   type    = string
-  default = "eastus"
+  default = "East US"
 }
 
 
+variable "managed_image_resource_group" {
+  type    = string
+  default = "RG-Packer-images"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "RG-Deployments"
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "golden-vnet"
+}
+
 variable "vm_name" {
   type    = string
-  default = "golden-grafana-image"
+  default = "golden-grafana-vm"
+}
+
+variable "size" {
+  type    = string
+  default = "Standard_DS2_v2"
 }
